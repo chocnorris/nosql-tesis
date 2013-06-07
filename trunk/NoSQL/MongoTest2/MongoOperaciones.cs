c0c1587aad921845b5da72e54e7bdecce44bf33c
falettiluciano@gmail.com <falettiluciano@gmail.com@9fb329a3-7608-f334-3418-16d0b1c297ea>
2013-06-07 00:45:44 +0000

    Idea que hablamos (interfaces para operaciones y entidades)

---
+++ trunk/NoSQL/MongoTest2/MongoOperaciones.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoTest2.Servicios;
+using MongoTest2.Entidades;
+using MongoTest2.Modelo;
+
+namespace MongoTest2
+{
+    public class MongoOperaciones : IOperaciones
+    {
+        /// <summary>
+        /// Inicializar base de datos
+        /// </summary>
+        public MongoOperaciones()
+        {
+
+        }
+        public List<Autor> GetAutores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comentario> GetComentarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Autor GetAutor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comentario GetComentario()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
