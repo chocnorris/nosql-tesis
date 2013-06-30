7203cce945df86f502431c948230a2299616f6a7
jcloglen@gmail.com <jcloglen@gmail.com@9fb329a3-7608-f334-3418-16d0b1c297ea>
2013-06-30 23:00:53 +0000

    Fotos

--- trunk/NoSQL/MongoTest2/Modelo/Author.cs
+++ trunk/NoSQL/MongoTest2/Modelo/Author.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MongoDB.Bson;
+using System.Drawing;
 
 namespace MongoTest2.Modelo
 {
@@ -11,5 +12,7 @@ namespace MongoTest2.Modelo
         public object Id { get; set; }
         public string Name { get; set; }
         //TODO: Agregar más información para los autores/usuarios del sistema
+
+        public Bitmap Photo { get; set; }
     }
 }
