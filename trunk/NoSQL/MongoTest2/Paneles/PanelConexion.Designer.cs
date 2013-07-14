6d70231dd3af6e990c3b94bf34fa9a346ce74d61
jcloglen@gmail.com <jcloglen@gmail.com@9fb329a3-7608-f334-3418-16d0b1c297ea>
2013-07-14 02:39:51 +0000

    Conexión un poco más robuste. Todavía falta un poco pero va bien.

---
+++ trunk/NoSQL/MongoTest2/Paneles/PanelConexion.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace NoSQL.Paneles
+{
+    partial class PanelConexion
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxHost = new System.Windows.Forms.ComboBox();
+            this.labelHost = new System.Windows.Forms.Label();
+            this.labelDB = new System.Windows.Forms.Label();
+            this.comboBoxDB = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxHost
+            // 
+            this.comboBoxHost.FormattingEnabled = true;
+            this.comboBoxHost.Location = new System.Drawing.Point(93, 44);
+            this.comboBoxHost.Name = "comboBoxHost";
+            this.comboBoxHost.Size = new System.Drawing.Size(203, 21);
+            this.comboBoxHost.TabIndex = 10;
+            this.comboBoxHost.Text = "localhost";
+            // 
+            // labelHost
+            // 
+            this.labelHost.AutoSize = true;
+            this.labelHost.Location = new System.Drawing.Point(12, 48);
+            this.labelHost.Name = "labelHost";
+            this.labelHost.Size = new System.Drawing.Size(29, 13);
+            this.labelHost.TabIndex = 8;
+            this.labelHost.Text = "Host";
+            // 
+            // labelDB
+            // 
+            this.labelDB.AutoSize = true;
+            this.labelDB.Location = new System.Drawing.Point(12, 20);
+            this.labelDB.Name = "labelDB";
+            this.labelDB.Size = new System.Drawing.Size(75, 13);
+            this.labelDB.TabIndex = 7;
+            this.labelDB.Text = "Base de datos";
+            // 
+            // comboBoxDB
+            // 
+            this.comboBoxDB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDB.FormattingEnabled = true;
+            this.comboBoxDB.Location = new System.Drawing.Point(93, 17);
+            this.comboBoxDB.Name = "comboBoxDB";
+            this.comboBoxDB.Size = new System.Drawing.Size(203, 21);
+            this.comboBoxDB.TabIndex = 6;
+            // 
+            // PanelConexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBoxHost);
+            this.Controls.Add(this.labelHost);
+            this.Controls.Add(this.labelDB);
+            this.Controls.Add(this.comboBoxDB);
+            this.Name = "PanelConexion";
+            this.Size = new System.Drawing.Size(331, 144);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxHost;
+        private System.Windows.Forms.Label labelHost;
+        private System.Windows.Forms.Label labelDB;
+        private System.Windows.Forms.ComboBox comboBoxDB;
+    }
+}
---
+++ trunk/NoSQL/MongoTest2/Paneles/PanelConexion.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NoSQL.Servicios;
+using System.Net.Sockets;
+
+namespace NoSQL.Paneles
+{
+    public partial class PanelConexion : UserControl
+    {
+        Ventana padre;
+        public PanelConexion(Ventana padre)
+        {
+            this.padre = padre;
+            InitializeComponent();
+            comboBoxDB.Items.Add("Mongo");
+            comboBoxDB.Items.Add("Cassandra");
+            comboBoxDB.Items.Add("MySQL");
+            comboBoxDB.SelectedIndex = 0;
+            comboBoxHost.Items.Add("localhost");
+            comboBoxHost.Items.Add("127.0.0.1");
+            comboBoxHost.Items.Add("192.168.56.201");
+            comboBoxHost.Items.Add("192.168.56.202");
+            comboBoxHost.Items.Add("192.168.56.203");
+            comboBoxHost.Items.Add("mongo201:27017,mongo202:27017,mongo203:27017/?replicaSet=rs0");
+
+        }
+
+        private bool testConnection(string host, int defport)
+        {
+            if (!Uri.IsWellFormedUriString(host, UriKind.RelativeOrAbsolute) || host.Equals(""))
+            {
+                MessageBox.Show("Host no válido", "Error");
+                return false;
+            }
+            if(!host.Contains("//"))
+                host = "mongodb://"+host;
+            int port = defport;
+            Uri uri = null;
+            try
+            {
+                uri = new Uri(host, UriKind.RelativeOrAbsolute);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se puede conectar", "Error");
+                return false;
+            }
+            if(uri.Port != -1)
+                port = uri.Port;
+            host = uri.Host;
+
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                try
+                {
+                    socket.Connect(host, port);
+                    socket.Close();
+                }
+                catch (SocketException ex)
+                {
+                    MessageBox.Show("No se puede conectar"+Environment.NewLine+ex.Message, "Error");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void Conectar()
+        {
+            UserControl panelAux = null;
+            IOperaciones db = null;
+            if (comboBoxDB.SelectedItem.ToString() == "Mongo")
+            {
+                if (!testConnection(comboBoxHost.Text, 27017))
+                    return;
+                MongoOperaciones md = new MongoOperaciones("forum", comboBoxHost.Text);
+                panelAux = new InfoMongo(md);
+                db = md;
+            }
+            if (comboBoxDB.SelectedItem.ToString() == "Cassandra")
+            {
+                if (!testConnection(comboBoxHost.Text, 9160))
+                    return;
+                db = new CassandraOperaciones("forum", comboBoxHost.Text);
+                db.Initialize(false);
+            }
+            if (comboBoxDB.SelectedItem.ToString() == "MySQL")
+            {
+                if (!testConnection(comboBoxHost.Text, 3306))
+                    return;
+                db = new MysqlOperaciones("forum", comboBoxHost.Text);
+            }
+            padre.AfterConnection(db, panelAux);
+        }
+    }
+}
