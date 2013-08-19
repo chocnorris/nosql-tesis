04c1c7aee325db0cdb8db0a1ac413e97d5b5db38
falettiluciano@gmail.com <falettiluciano@gmail.com@9fb329a3-7608-f334-3418-16d0b1c297ea>
2013-08-19 03:07:45 +0000

    GUI - Chart con estadistica de inserción de comentarios aleatorios

--- NoSQL/MongoTest2/VentanasAux/VentanaRandom.Designer.cs
+++ NoSQL/MongoTest2/VentanasAux/VentanaRandom.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.buttonAutores = new System.Windows.Forms.Button();
             this.buttonThreads = new System.Windows.Forms.Button();
             this.buttonCom = new System.Windows.Forms.Button();
@@ -39,10 +41,12 @@
             this.buttonCom1MB = new System.Windows.Forms.Button();
             this.numericUpDownCom1MB = new System.Windows.Forms.NumericUpDown();
             this.labelEstado = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAutores)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownThreads)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCom)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCom1MB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonAutores
@@ -151,11 +155,30 @@
             this.labelEstado.Size = new System.Drawing.Size(0, 13);
             this.labelEstado.TabIndex = 7;
             // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.chart1.BackSecondaryColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(296, 14);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Chocolate;
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(501, 208);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
             // VentanaRandom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(271, 160);
+            this.ClientSize = new System.Drawing.Size(809, 253);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.labelEstado);
             this.Controls.Add(this.numericUpDownCom1MB);
             this.Controls.Add(this.buttonCom1MB);
@@ -172,10 +195,12 @@
             this.Name = "VentanaRandom";
             this.Text = "Datos aleatorios";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.VentanaRandom_FormClosing);
+            this.Load += new System.EventHandler(this.VentanaRandom_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAutores)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownThreads)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCom)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCom1MB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,5 +219,6 @@
         private System.Windows.Forms.Button buttonCom1MB;
         private System.Windows.Forms.NumericUpDown numericUpDownCom1MB;
         private System.Windows.Forms.Label labelEstado;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
