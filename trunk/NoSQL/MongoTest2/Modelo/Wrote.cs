83ea7af15782707afd7329f5f9fc6560a75580ef
falettiluciano@gmail.com <falettiluciano@gmail.com@9fb329a3-7608-f334-3418-16d0b1c297ea>
2013-08-28 03:29:48 +0000

    Neo4j

---
+++ trunk/NoSQL/MongoTest2/Modelo/Wrote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Neo4jClient;
+
+namespace NoSQL.Modelo
+{
+    public class Wrote : Relationship<Payload>, IRelationshipAllowingSourceNode<Author>,
+ IRelationshipAllowingTargetNode<Comment>
+    {
+        public static readonly string TypeKey = "WROTE";
+
+        public string Caption { get; set; }
+
+        public Wrote(NodeReference targetNode, Payload payload)
+            : base(targetNode, payload)
+        { }
+
+        public override string RelationshipTypeKey
+        {
+            get { return TypeKey; }
+        }
+    }
+}
