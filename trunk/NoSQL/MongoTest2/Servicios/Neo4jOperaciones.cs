30dcf0f406d20e5f88cdcb507ab797ccda523e49
jcloglen@gmail.com <jcloglen@gmail.com@9fb329a3-7608-f334-3418-16d0b1c297ea>
2013-08-29 19:04:04 +0000

    Neo4j - Bastante avanzado

--- trunk/NoSQL/MongoTest2/Servicios/Neo4jOperaciones.cs
+++ trunk/NoSQL/MongoTest2/Servicios/Neo4jOperaciones.cs
@@ -52,7 +52,10 @@ namespace NoSQL.Servicios
             var res = client
                 .Cypher 
                 .Start(new { n = Node.ByIndexQuery("Author", "*:*") })
-                .Return<Node<Author>>("n");
+                .Return<Node<Author>>("n")
+                .Skip(skip);
+            if (take != 0)
+                res = res.Limit(take);
             List<Node<Author>> lista = res.Results.ToList();
             List<Author> ret = new List<Author>();
             Author aut;
@@ -70,67 +73,30 @@ namespace NoSQL.Servicios
             var res = client
                 .Cypher
                 .Start(new { n = Node.ByIndexQuery("Comment", "*:*") })
-                .Return<Node<Comment>>("n");
+                .Return<Node<Comment>>("n").Skip(skip);
+            if (take != 0)
+                res = res.Limit(take);
             List<Node<Comment>> lista = res.Results.ToList();
             List<Comment> ret = new List<Comment>();
-            Comment com;
             foreach (Node<Comment> node in lista)
             {
-                com = node.Data;
-                com.Id = node.Reference.Id;
-                var aut = client
-                    .Cypher
-                    .Start(new { comment = new NodeReference((long)com.Id) })
-                    .Match("(comment)<-[:WROTE]-(author)")
-                    .Return<Node<Author>>("author");
-                com.Author = new Author()
-                {
-                    Id = aut.Results.FirstOrDefault().Reference.Id,
-                    Name = aut.Results.FirstOrDefault().Data.Name
-                };
-                var par = client
-                    .Cypher
-                    .Start(new { comment = new NodeReference((long)com.Id) })
-                    .Match("(parent)<-[:PARENT]-(comment)")
-                    .Return<Node<Comment>>("parent").Results.FirstOrDefault();
-                com.Parent_id = par.Reference.Id;
-                var thr = client
-                    .Cypher
-                    .Start(new { comment = new NodeReference((long)com.Id) })
-                    .Match("(thread)<-[:COMMTH]-(comment)")
-                    .Return<Node<Comment>>("thread").Results.FirstOrDefault();
-                com.Thread_id = thr.Reference.Id;
-                ret.Add(com);
+                ret.Add(singleComment(node));
             }
             return ret;
         }
 
         public List<Comment> GetChildComments(object Parent_id)
         {
-            if (Parent_id.GetType().Equals(typeof(string)))
-                Parent_id = long.Parse((string)Parent_id);
+            Parent_id = long.Parse(Parent_id.ToString());
             var lista = client
             .Cypher
             .Start(new { parent = new NodeReference((long)Parent_id) })
             .Match("(parent)<-[:PARENT]-(comment)")
             .Return<Node<Comment>>("comment").Results.ToList();
             List<Comment> ret = new List<Comment>();
-            Comment com;
             foreach (Node<Comment> node in lista)
             {
-                com = node.Data;
-                com.Id = node.Reference.Id;
-                var aut = client
-                    .Cypher
-                    .Start(new { comment = new NodeReference((long)com.Id) })
-                    .Match("(comment)<-[:WROTE]-(author)")
-                    .Return<Node<Author>>("author");
-                com.Author = new Author()
-                {
-                    Id = aut.Results.FirstOrDefault().Reference.Id,
-                    Name = aut.Results.FirstOrDefault().Data.Name
-                };
-                ret.Add(com);
+                ret.Add(singleComment(node));
             }
             return ret;
         }
@@ -140,34 +106,28 @@ namespace NoSQL.Servicios
             var res = client
                 .Cypher
                 .Start(new { n = Node.ByIndexQuery("Thread", "*:*") })
-                .Return<Node<Thread>>("n");
+                .Return<Node<Thread>>("n")
+                .Skip(skip);
+            if (take != 0)
+                res = res.Limit(take);
             List<Node<Thread>> lista = res.Results.ToList();
             List<Thread> ret = new List<Thread>();
-            Thread thr;
             foreach (Node<Thread> node in lista)
             {
-                thr = node.Data;
-                thr.Id = node.Reference.Id;
-                var aut = client
-                    .Cypher
-                    .Start(new { thread = new NodeReference((long)thr.Id)})
-                    .Match("(thread)<-[:WROTE]-(author)")
-                    .Return<Node<Author>>("author");
-                thr.Author = new Author()
-                {
-                    Id = aut.Results.FirstOrDefault().Reference.Id,
-                    Name = aut.Results.FirstOrDefault().Data.Name
-                };
-                ret.Add(thr);
+                ret.Add(singleThread(node));
             }
             return ret;
         }
 
         public Thread GetThread(object id)
         {
-            if (id.GetType().Equals(typeof(string)))
-                id = long.Parse((string)id);
+            id = long.Parse(id.ToString());
             var node = client.Get<Thread>(new NodeReference((long)id));
+            return singleThread(node);
+        }
+
+        private Thread singleThread(Node<Thread> node)
+        {
             Thread thr = node.Data;
             thr.Id = node.Reference.Id;
             var aut = client
@@ -180,13 +140,20 @@ namespace NoSQL.Servicios
                 Id = aut.Results.FirstOrDefault().Reference.Id,
                 Name = aut.Results.FirstOrDefault().Data.Name,
             };
+
+            long respuestas = client
+                .Cypher
+                .Start(new { thread = new NodeReference((long)thr.Id) })
+                .Match("(thread)<-[:PARENT]-(comment)")
+                .Return(() => All.Count()).Results.FirstOrDefault();
+
+            thr.CommentCount = respuestas;
             return thr;
         }
 
         public Author GetAuthor(object id)
         {
-            if (id.GetType().Equals(typeof(string)))
-                id = long.Parse((string)id);
+            id = long.Parse(id.ToString());
             var node = client.Get<Author>(new NodeReference((long)id));
             Author aut = node.Data;
             aut.Id = node.Reference.Id;
@@ -195,9 +162,13 @@ namespace NoSQL.Servicios
 
         public Comment GetComment(object id)
         {
-            if (id.GetType().Equals(typeof(string)))
-                id = long.Parse((string)id);
+            id = long.Parse(id.ToString());
             var node = client.Get<Comment>(new NodeReference((long)id));
+            return singleComment(node);
+        }
+
+        private Comment singleComment(Node<Comment> node)
+        {
             Comment com = node.Data;
             com.Id = node.Reference.Id;
             var aut = client
@@ -222,6 +193,14 @@ namespace NoSQL.Servicios
                 .Match("(thread)<-[:COMMTH]-(comment)")
                 .Return<Node<Comment>>("thread").Results.FirstOrDefault();
             com.Thread_id = thr.Reference.Id;
+
+            long respuestas = client
+            .Cypher
+            .Start(new { parent = new NodeReference((long)com.Id) })
+            .Match("(parent)<-[:PARENT]-(comment)")
+            .Return(() => All.Count()).Results.FirstOrDefault();
+
+            com.CommentCount = respuestas;
             return com;
         }
 
@@ -280,17 +259,29 @@ namespace NoSQL.Servicios
 
         public long GetAuthorsCount()
         {
-            return GetAuthors().Count;
+            var res = client
+            .Cypher
+            .Start(new { n = Node.ByIndexQuery("Author", "*:*") })
+            .Return(() => All.Count());
+            return res.Results.First();
         }
 
         public long GetThreadsCount()
         {
-            return GetThreads().Count;
+            var res = client
+            .Cypher
+            .Start(new { n = Node.ByIndexQuery("Thread", "*:*") })
+            .Return(() => All.Count());
+            return res.Results.First();
         }
 
         public long GetCommentsCount()
         {
-            return GetComments().Count;
+            var res = client
+            .Cypher
+            .Start(new { n = Node.ByIndexQuery("Comment", "*:*") })
+            .Return(() => All.Count());
+            return res.Results.First();
         }
 
         public bool RemoveAuthor(Author autor)
@@ -304,11 +295,6 @@ namespace NoSQL.Servicios
         }
 
         public bool RemoveComment(Comment comentario)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int ThradsByAuthor(object id)
         {
             throw new NotImplementedException();
         }
